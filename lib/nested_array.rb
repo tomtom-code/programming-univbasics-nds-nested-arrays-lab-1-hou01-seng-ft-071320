2ad41b3554ed415894e3f9d16c63f7e40fba8aba
scrappy-strategy-4846 <tomlai8882000@hotmail.com>
2020-06-12 22:40:31 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -41,7 +41,7 @@ end
 def matrix_lookup(matrix, row, column)
   # Given any matrix (array of arrays), a row index and a column index, 
   # Return the matrix's content at that row and and column
-  
+  storage_room[0][1]
   
 end
 
